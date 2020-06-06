78669068cabdcb2e6599ced6a85fa1f4c9b3f8ea
rafimk <rafi.genius.cs@gmail.com>
2020-06-06 22:03:52 +0400

    Change app text with test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Bye there/i);
+  const linkElement = getByText(/I was changed on the Feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 
